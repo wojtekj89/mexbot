db7ceee183a4d8a066333dd2ba81a079984d0e2f
Wojtek <wojtekj1989@gmail.com>
2018-12-14 16:08:22 +0100

    scaled order generator

--- Mexbot.py
+++ Mexbot.py
@@ -1,24 +1,47 @@
-import BitmexAPIConnector
+import BitmexTicker
 import threading
 import time
+import logging
+import requests
 
-class Mexbot:
-    
+
+class Mexbot():
+    # Main class of application. Starting all threads, storing data and routing all events
+
     def __init__(self):
-        self.ticker = None
+        self.logger = setup_logger()
+        self.logger.info("Starting Mexbot")
         self.symbol = "XBTUSD"
-        mexAPIThread = threading.Thread(target=BitmexAPIConnector.run, args=[self])
-        mexAPIThread.start()
-        # mex = BitmexAPIConnector.run()
+        self.ticker = None
+        self.mexbotTicker = threading.Thread(
+            target=BitmexTicker.run, args=[self])
+        self.mexbotTicker.start()
+        # # Prepare HTTPS session
+        # self.session = requests.Session()
+        # # These headers are always sent
+        # self.session.headers.update({'user-agent': 'mexbot-' + constants.VERSION})
+        # self.session.headers.update({'content-type': 'application/json'})
+        # self.session.headers.update({'accept': 'application/json'})
 
     def updateTicker(self, ticker):
         self.ticker = ticker
 
 
+def setup_logger():
+    # Prints logger info to terminal
+    logger = logging.getLogger()
+    # Change this to DEBUG if you want a lot more info
+    logger.setLevel(logging.INFO)
+    ch = logging.StreamHandler()
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    return logger
+
+
 if __name__ == "__main__":
-    print("starting mexbot")
     mexbot = Mexbot()
     while (True):
-        print("in ticker loop")
-        time.sleep(10)
-        print(mexbot.ticker)
+        time.sleep(5)
+        mexbot.logger.info(mexbot.ticker)
